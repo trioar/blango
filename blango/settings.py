6a923ec2cad4fe5aba047f7d327bd1fb3a9517a2
Aref Shaheed <aref.shaheed.sy@gmail.com>
2021-11-22 12:23:08 +0000

    Finish html frameworks

--- blango/settings.py
+++ blango/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-+sn%dpa!086+g+%44z9*^j^q-u4n!j(#wl)x9a%_1op@zz2+1-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nitro-traffic-8000.codio.io']
 
 
 # Application definition
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'blango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- blango/urls.py
+++ blango/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# other imports
+import blog.views
 
 urlpatterns = [
+    path("", blog.views.index),
     path('admin/', admin.site.urls),
 ]
